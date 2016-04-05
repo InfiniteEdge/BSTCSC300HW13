761bbe77bd56acb2d71d759c9e41ff05e7ce5915
Nick Karsten <nicholas.karsten@cuw.edu>
2016-04-05 10:08:36 -0500

    rebalance left left and right right

---
+++ app/src/main/java/com/example/awesomefat/bst_csc300_spring2016/BSTCore.java
@@ -0,0 +1,12 @@
+package com.example.awesomefat.bst_csc300_spring2016;
+
+/**
+ * Created by Nick on 4/4/2016.
+ */
+public class BSTCore
+{
+    static String outOfBalanceInitial;
+    static String outOfBalanceSecondarily;
+    static BinaryTree tempTree;
+}
+
---
+++ app/src/main/java/com/example/awesomefat/bst_csc300_spring2016/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.awesomefat.bst_csc300_spring2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BST tree = new BST();
+        String vals = "fgbbb";
+        for(int i = 0; i < vals.length(); i++)
+        {
+            tree.add(vals.charAt(i));
+        }
+
+        System.out.println("Out of balance: " + tree.isOutOfBalance());
+        tree.howAreWeOutOfBalance('b');
+
+        if(tree.isOutOfBalance())
+        {
+            tree.rebalanceTree(BSTCore.outOfBalanceInitial, BSTCore.outOfBalanceSecondarily);
+        }
+
+        System.out.println("Out of balance: " + tree.isOutOfBalance());
+
+
+
+
+        //tree.visitPreOrder();
+        //tree.visitPostOrder();
+        //tree.visitInOrder();
+        //tree.visitLevelOrder();
+    }
+}
